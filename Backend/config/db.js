bda322090bdfb0b3696907aa499a7f5d36901a3a
briankarani67 <briankarani884@gmail.com>
2026-04-25 02:35:43 +0300

    Eccom

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,17 +1,4 @@
-// const mysql = require('mysql2');
-// require('dotenv').config();
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
-
-// module.exports = pool.promise();
 
 const mysql = require('mysql2');
 const path = require('path');
@@ -25,10 +12,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 module.exports = pool.promise();
