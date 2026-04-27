9b4cd4cb8f212f6366a7b2c83413f41cd48596ea
briankarani67 <briankarani884@gmail.com>
2026-04-27 16:09:44 +0300

    Eccom

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,18 +7,30 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 console.log("Checking DB_PASSWORD:", process.env.DB_PASSWORD); 
 
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   ssl:{
+//     rejectUnauthorized:false
+//   }
+// });
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  // If DB_PORT isn't found in the environment, use 3306
+  port: process.env.DB_PORT || 3306, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl:{
-    rejectUnauthorized:false
-  }
+  ssl: process.env.DB_HOST === 'localhost' ? false : { rejectUnauthorized: false }
 });
 
 module.exports = pool.promise();
