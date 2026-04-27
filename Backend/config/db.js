4a819bfd12de1b22430f2ff8b7a62cba0caf352f
briankarani67 <briankarani884@gmail.com>
2026-04-27 15:10:44 +0300

    Eccom

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,7 @@ const path = require('path');
 // This forces Node to look in the folder above 'config' for the .env
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-console.log("Checking DB_PASSWORD:", process.env.DB_PASSWORD); // This should print @Karani884
+console.log("Checking DB_PASSWORD:", process.env.DB_PASSWORD); 
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
